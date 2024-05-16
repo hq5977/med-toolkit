07298c22f5c00aa3052e111be6a90f4ea71f4344
黄强 <huangqiang@chexiao.co>
2024-05-16 15:14:34 +0800

    Initial commit

---
+++ src/main/java/com/joey/core/MedToolkitApplication.java
@@ -0,0 +1,13 @@
+package com.joey.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedToolkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedToolkitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joey/core/MedToolkitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joey.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedToolkitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
